072bfeb3562c11b3b1c576f723cecd4bd2dfb7fa
choiseongwook11 <a4bingo3593@gmail.com>
2025-05-22 00:50:05 +0900

    feat: 메인화면 퍼블리싱
    - asset 추가
    - Module.css 적용

---
+++ src/assets/index.ts
@@ -0,0 +1,37 @@
+import GBSM_Symbol from "./img/gbsm-symbol.png";
+import GBSWFULLSCREEN from "./img/gbsm-full-symbol.png";
+import GBSMLOGO from "./img/gbsm-symbol-new-solid.png";
+import SchoolFullImg from "./img/SchoolFull.png";
+import Error from "./img/error.png";
+import Info from "./img/info.png";
+import Background from "./img/background-img.png"
+import Edit from "./img/edit-img.png"
+import Success from "./img/success.png";
+import ErrorCharacter from "./img/NotFound.png";
+import MeisterCharacter from "./img/meister-character.png";
+import SWCharacter from "./img/meister-game-character.png";
+import GameCharacter from "./img/meister-sw-character.png";
+import StepOne from "./img/step-one.png";
+import StepTwo from "./img/step-two.png";
+import StepThree from "./img/step-three.png";
+import Check from "./img/check.png"
+
+export {
+  GBSM_Symbol,
+  GBSWFULLSCREEN,
+  GBSMLOGO,
+  SchoolFullImg,
+  Error,
+  Info,
+  Success,
+  Background,
+  Edit,
+  ErrorCharacter,
+  MeisterCharacter,
+  SWCharacter,
+  GameCharacter,
+  StepOne,
+  StepTwo,
+  StepThree,
+  Check,
+};
