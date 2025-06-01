45b494516dc1ec41ee5bfb180953b4e967ea03aa
806gw <shjea4364@naver.com>
2025-06-02 00:25:39 +0900

    feat: 실습실 대여하기 성공 페이지 개발

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,39 +1,41 @@
-import GBSM_Symbol from "./img/gbsm-symbol.png";
-import GBSWFULLSCREEN from "./img/gbsm-full-symbol.png";
-import GBSMFULLSCREEN from "./img/gbsm-full-symbol-green.png";
-import GBSMLOGO from "./img/gbsm-symbol-new-solid.png";
-import SchoolFullImg from "./img/SchoolFull.png";
-import Error from "./img/error.png";
-import Info from "./img/info.png";
-import Background from "./img/background-img.png"
-import Edit from "./img/edit-img.png"
-import Success from "./img/success.png";
-import ErrorCharacter from "./img/NotFound.png";
-import MeisterCharacter from "./img/meister-character.png";
-import SWCharacter from "./img/meister-game-character.png";
-import GameCharacter from "./img/meister-sw-character.png";
-import StepOne from "./img/step-one.png";
-import StepTwo from "./img/step-two.png";
-import StepThree from "./img/step-three.png";
-import Check from "./img/check.png"
+import GBSM_Symbol from './img/gbsm-symbol.png'
+import GBSWFULLSCREEN from './img/gbsm-full-symbol.png'
+import GBSMFULLSCREEN from './img/gbsm-full-symbol-green.png'
+import GBSMLOGO from './img/gbsm-symbol-new-solid.png'
+import SchoolFullImg from './img/SchoolFull.png'
+import Error from './img/error.png'
+import Info from './img/info.png'
+import Background from './img/background-img.png'
+import Edit from './img/edit-img.png'
+import Success from './img/success.png'
+import ErrorCharacter from './img/NotFound.png'
+import MeisterCharacter from './img/meister-character.png'
+import SWCharacter from './img/meister-game-character.png'
+import GameCharacter from './img/meister-sw-character.png'
+import StepOne from './img/step-one.png'
+import StepTwo from './img/step-two.png'
+import StepThree from './img/step-three.png'
+import Check from './img/check.png'
+import SuccessCheck from './img/SuccessCheck.png'
 
 export {
-  GBSM_Symbol,
-  GBSWFULLSCREEN,
-  GBSMFULLSCREEN,
-  GBSMLOGO,
-  SchoolFullImg,
-  Error,
-  Info,
-  Success,
-  Background,
-  Edit,
-  ErrorCharacter,
-  MeisterCharacter,
-  SWCharacter,
-  GameCharacter,
-  StepOne,
-  StepTwo,
-  StepThree,
-  Check,
-};
+	GBSM_Symbol,
+	GBSWFULLSCREEN,
+	GBSMFULLSCREEN,
+	GBSMLOGO,
+	SchoolFullImg,
+	Error,
+	Info,
+	Success,
+	Background,
+	Edit,
+	ErrorCharacter,
+	MeisterCharacter,
+	SWCharacter,
+	GameCharacter,
+	StepOne,
+	StepTwo,
+	StepThree,
+	Check,
+	SuccessCheck,
+}
